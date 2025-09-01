47f7822725ca20334c7b75e9dd6bf977a75e35b5
Damien Sawyer <damiensawyer@gmail.com>
2025-09-01 16:55:17 +1000

    init

---
+++ Sam.Coach.Tests/UnitTests.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Sam.Coach.Tests
+{
+    public class UnitTests
+    {
+        [Theory]
+        [InlineData(new [] {4,3,5,8,5,0,0,-3}, new [] {3,5,8})]
+        [InlineData(new [] {4,6,-3,3,7,9}, new [] {-3,3,7,9})]
+        [InlineData(new [] {9,6,4,5,2,0}, new [] {4,5})]
+        [InlineData(new [] {1,2,3,4,5}, new [] {1,2,3,4,5})]
+        [InlineData(new [] {5,4,3,2,1}, new [] {5})]
+        [InlineData(new int[] {}, new int[] {})]
+        [InlineData(new [] {42}, new [] {42})]
+        [InlineData(new [] {1,1,1,1}, new [] {1})]
+        [InlineData(new [] {1,3,2,4,6,5,7,9,8}, new [] {2,4,6})]
+        public async Task Can_Find(IEnumerable<int> data, IEnumerable<int> expected)
+        {
+            var finder = new LongestRisingSequenceFinder();
+            var actual = await finder.Find(data);
+
+            actual.Should().Equal(expected);
+        }
+    }
+}
---
+++ Sam.Coach/LongestRisingSequenceFinder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sam.Coach
+{
+    public class LongestRisingSequenceFinder : ILongestRisingSequenceFinder
+    {
+        public Task<IEnumerable<int>> Find(IEnumerable<int> numbers) => Task.Run(() =>
+        {
+            if (numbers == null)
+                return new List<int>();
+
+            var numbersList = numbers.ToList();
+            if (numbersList.Count <= 1)
+                return numbersList;
+
+            var longestSequence = new List<int>();
+            var currentSequence = new List<int> { numbersList[0] };
+
+            for (int i = 1; i < numbersList.Count; i++)
+            {
+                if (numbersList[i] > numbersList[i - 1])
+                {
+                    currentSequence.Add(numbersList[i]);
+                }
+                else
+                {
+                    if (currentSequence.Count > longestSequence.Count)
+                    {
+                        longestSequence = new List<int>(currentSequence);
+                    }
+                    currentSequence = new List<int> { numbersList[i] };
+                }
+            }
+
+            if (currentSequence.Count > longestSequence.Count)
+            {
+                longestSequence = currentSequence;
+            }
+
+            return longestSequence.AsEnumerable();
+        });
+    }
+}
